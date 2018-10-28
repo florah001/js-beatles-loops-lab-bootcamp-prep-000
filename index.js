64e49ad8d6de47112455fa5fa52be751a93908e1
florah001 <florah001@yahoo.co.uk>
2018-10-28 15:19:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+function theBeatlesPlay(array) {
+  var i = ();
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+for (i = 0; i < 5; i++) {
+    text += musicians + "plays " + instruments + "<br>";
+}
+  return (["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);  
+}
+
+function iLoveTheBeatles() {
+    
+    return 'I love the Beatles!';
+  } 
